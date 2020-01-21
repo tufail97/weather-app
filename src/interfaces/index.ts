30e983fe5e6655a893375fececc9c7cbbedf8887
tufail97 <tufail.rahman@adylic.com>
2020-01-21 20:18:44 +0000

    created basic respose interface

---
+++ src/interfaces/index.ts
@@ -0,0 +1,33 @@
+interface ICity {
+  id: number;
+  name: string;
+  country: string;
+}
+
+interface IList {
+  dt: number;
+  temp: {
+    day: number;
+    min: number;
+    max: number;
+    night: number;
+    eve: number;
+    morn: number;
+  };
+  feels_like: {
+    day: number;
+    night: number;
+    eve: number;
+    morn: number;
+  };
+  pressure: number;
+  humidity: number;
+}
+
+export interface IWeatherResponse {
+  city: ICity;
+  cod: string;
+  message: number | string;
+  cnt: number;
+  list: Array<IList>;
+}
